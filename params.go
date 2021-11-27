c80db7329af585e64403bca7095af37d82084c1e
marjune <marjune@163.com>
2021-11-27 15:16:56 +0800

    feat(params/validate): validate IP conflict with wildcard IP

--- params.go
+++ params.go
@@ -3,29 +3,32 @@ package goVirtualHost
 import "fmt"
 
 func (params params) validateParam(param *param) (errs []error) {
-	proto := param.proto
-	addr := param.ip
-	hostnames := param.hostNames
-
 	for _, ownParam := range params {
 		if ownParam == param {
 			continue
 		}
 
-		if ownParam.proto != proto || ownParam.ip != addr {
-			continue
+		if ownParam.port == param.port && ownParam.proto != "unix" && param.proto != "unix" {
+			if (ownParam.proto == "tcp" && ownParam.ip == "") ||
+				(param.proto == "tcp" && param.ip == "") ||
+				(ownParam.proto == param.proto && (ownParam.ip == "" || param.ip == "")) {
+				err := fmt.Errorf("conflict IP address: %+v, %+v", ownParam, param)
+				errs = append(errs, err)
+			}
 		}
 
-		ownUseTLS := ownParam.cert != nil
-		inputUseTLS := param.cert != nil
-		if ownUseTLS != inputUseTLS {
-			err := fmt.Errorf("cannot serve for both Plain and TLS mode: %+v", param)
-			errs = append(errs, err)
-		}
+		if ownParam.proto == param.proto && ownParam.ip == param.ip && ownParam.port == param.port {
+			ownUseTLS := ownParam.cert != nil
+			useTLS := param.cert != nil
+			if ownUseTLS != useTLS {
+				err := fmt.Errorf("cannot serve for both Plain and TLS mode: %+v", param)
+				errs = append(errs, err)
+			}
 
-		if (len(hostnames) == 0 && len(ownParam.hostNames) == 0) || (ownParam.hasHostNames(hostnames)) {
-			err := fmt.Errorf("duplicated address and hostname: %+v", param)
-			errs = append(errs, err)
+			if (len(param.hostNames) == 0 && len(ownParam.hostNames) == 0) || (ownParam.hasHostNames(param.hostNames)) {
+				err := fmt.Errorf("duplicated address and hostname: %+v", param)
+				errs = append(errs, err)
+			}
 		}
 	}
 
